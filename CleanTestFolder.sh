27256336c32fcf857669e3e368a8f631febc0bd6
Simon <simon.thevenin@live.fr>
2018-06-11 17:18:08 -0400

    Add SDDP algorithm

---
+++ CleanTestFolder.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#
+echo 'Clean Repository ...'
+rm ./Test/Bounds/*
+rm ./Test/SolveInfo/*
+rm ./Test/Statistic/*
+rm ./Test/*
+rm ./Solutions/*
+rm ./Temp/*
+rm ./Evaluations/*
+rm ./Jobs/*
+rm ../../log/*
