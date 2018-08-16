98a0667fb22bd15618f6e2265ce7d5ced8b02418
Simon <simon.thevenin@live.fr>
2018-08-16 12:36:20 -0400

    solve some issue with the printing of the results

--- Constants.py
+++ Constants.py
@@ -51,7 +51,7 @@ class Constants( object ):
     PrintOnlyFirstStageDecision = False
     RunEvaluationInSeparatedJob = False
     PrintScenarios = False
-    PrintSolutionFileInTMP = False
+    PrintSolutionFileInTMP = True
     LauchEvalAfterSolve = True
 
     #Code parameter
