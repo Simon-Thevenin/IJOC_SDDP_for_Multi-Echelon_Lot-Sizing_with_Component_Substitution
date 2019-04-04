043bf71467c5fb8ae06e2167835631edf8a204b7
simon <simon.thevenin@imt-atlantique.fr>
2019-04-04 17:02:36 +0200

    Add Hybrid PH SDDP

--- Constants.py
+++ Constants.py
@@ -53,7 +53,7 @@ class Constants( object ):
     PrintOnlyFirstStageDecision = True
     RunEvaluationInSeparatedJob = False
     PrintScenarios = False
-    PrintSolutionFileInTMP = False
+    PrintSolutionFileInTMP = True
     LauchEvalAfterSolve = False
 
     #Code parameter
