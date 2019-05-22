f5200022b0fb2df2b7796f436ad538ee1cd22d79
simon <simon.thevenin@imt-atlantique.fr>
2019-05-22 09:32:03 +0200

    ML Local Search / Hybrid PH-SDDP/ HeuristicYFixSDDP/SDDP

--- scm.py
+++ scm.py
@@ -194,7 +194,7 @@ if __name__ == '__main__':
         #instance.DefineAsTwoItemIntance()
         # instance.ReadFromFile("K0011525", "NonStationary", "Normal")
 
-        GenerateInstances()
+        #GenerateInstances()
 
         instance.ReadInstanceFromExelFile(TestIdentifier.InstanceName)
         #instance.DrawSupplyChain()
