db4203552528c3b6d2826285060fb3f8e3492974
simon <simon.thevenin@imt-atlantique.fr>
2019-06-20 15:24:13 +0200

    ML Local Search / Hybrid PH-SDDP/ HeuristicYFixSDDP/SDDP

--- CreateScripts.py
+++ CreateScripts.py
@@ -162,7 +162,7 @@ srun python scm.py  Solve %s %s %s RQMC -n 5000 -p Re-solve -m PH  >/home/LS2N/t
     return qsub_filename
 
 if __name__ == "__main__":
-    csvfile = open("./Instances/InstancesToSolveH.csv", 'rb')
+    csvfile = open("./Instances/InstancesToSolveM.csv", 'rb')
     data_reader = csv.reader(csvfile, delimiter=",", skipinitialspace=True)
     instancenameslist = []
     for row in data_reader:
@@ -191,14 +191,14 @@ if __name__ == "__main__":
                         #fileheur.write("sbatch %s \n" % (jobname))
                         #jobname = CreateSDDPJob(instance, nrback, nrforward, setting, model="YFix")
                         #fileheur.write("sbatch %s \n" % (jobname))
-                        #jobname = CreateMLLocalSearchJob(instance, nrback, nrforward, setting, model="YFix")
-                        #fileheur.write("sbatch %s \n" % (jobname))
+                        jobname = CreateMLLocalSearchJob(instance, nrback, nrforward, setting, model="YFix")
+                        fileheur.write("sbatch %s \n" % (jobname))
                         #jobname = CreateMLLocalSearchJob(instance, nrback, nrforward, setting, model="YFix",mlsetting="NrIterationBeforeTabu1000")
                         #fileheur.write("sbatch %s \n" % (jobname))
                         #jobname = CreateMIPJob(instance, 100, model="YQFix")
                         #fileheur.write("sbatch %s \n" % (jobname))
-                        jobname = CreateHybridSearchJob(instance, nrback, nrforward, setting, model="YFix")
-                        fileheur.write("sbatch %s \n" % (jobname))
+                        #jobname = CreateHybridSearchJob(instance, nrback, nrforward, setting, model="YFix")
+                        #fileheur.write("sbatch %s \n" % (jobname))
 
 
                         #    fileheur.write("sbatch %s \n" % (jobname))
