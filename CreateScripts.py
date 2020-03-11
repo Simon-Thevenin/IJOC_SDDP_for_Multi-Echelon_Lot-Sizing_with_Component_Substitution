91e6e1385878a10f045102c57c0c27e2a4e14bf9
simon <simon.thevenin@imt-atlantique.fr>
2020-03-11 20:04:55 +0100

    ML Local Search / Hybrid PH-SDDP/ HeuristicYFixSDDP/SDDP

--- CreateScripts.py
+++ CreateScripts.py
@@ -216,22 +216,20 @@ if __name__ == "__main__":
                     for setting in ["Default"]:
                         nrforward = 1
 
-                        #jobname = CreateSDDPJob(instance, nrback, nrforward, setting, model="HeuristicYFix")
-                        #fileheur.write("sbatch %s \n" % (jobname))
-                        #jobname = CreateSDDPJob(instance, nrback, nrforward, setting, model="YFix")
-                        #fileheur.write("sbatch %s \n" % (jobname))
-                        #jobname = CreateMLLocalSearchJob(instance, nrback, nrforward, setting, model="YFix")
-                        #fileheur.write("sbatch %s \n" % (jobname))
-                        #jobname = CreateMLLocalSearchJob(instance, nrback, nrforward, setting, model="YFix",mlsetting="NrIterationBeforeTabu1000")
-                        #fileheur.write("sbatch %s \n" % (jobname))
-                        jobname = CreateMIPJob(instance, 100, model="YQFix")
+                        jobname = CreateSDDPJob(instance, nrback, nrforward, setting, model="HeuristicYFix")
                         fileheur.write("sbatch %s \n" % (jobname))
-                        #jobname = CreateHybridSearchJob(instance, nrback, nrforward, setting, model="YFix")
+                        jobname = CreateMLLocalSearchJob(instance, nrback, nrforward, setting, model="YFix",mlsetting="NrIterationBeforeTabu1000")
                         fileheur.write("sbatch %s \n" % (jobname))
-                        jobname = CreateMIPJob(instance, "6400b", model="YFix")
+                        #jobname = CreateMIPJob(instance, 100, model="YQFix")
+                        #fileheur.write("sbatch %s \n" % (jobname))
+                        jobname = CreateHybridSearchJob(instance, nrback, nrforward, setting, model="YFix")
                         fileheur.write("sbatch %s \n" % (jobname))
-                        jobname = CreateMIPJob(instance, "all2", model="YFix")
-                        fileheur.write("sbatch %s \n" % (jobname))
+                        #jobname = CreateMIPJob(instance, "6400b", model="YFix")
+                        #fileheur.write("sbatch %s \n" % (jobname))
+                        #jobname = CreateMIPJob(instance, "all2", model="YFix")
+                        #fileheur.write("sbatch %s \n" % (jobname))
+
+
 
                         #    fileheur.write("sbatch %s \n" % (jobname))
                         #for mlsetting in ["NrIterationBeforeTabu10", "NrIterationBeforeTabu50", "NrIterationBeforeTabu100", "NrIterationBeforeTabu1000"]:
