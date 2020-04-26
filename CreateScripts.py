a92787b3307f7179a1ff52473e0111eb06fdab61
simon <simon.thevenin@imt-atlantique.fr>
2020-04-26 16:00:21 +0200

    ML Local Search / Hybrid PH-SDDP/ HeuristicYFixSDDP/SDDP

--- CreateScripts.py
+++ CreateScripts.py
@@ -194,14 +194,14 @@ if __name__ == "__main__":
                               or "H08" in instance and nrback == "all5"
                               or "H010" in instance and nrback == "all5"
                     ) ) :
-                            #jobname = CreateMIPJob(instance, nrback, model="YFix", mipsetting=setting)
-                            #filenew.write("sbatch %s \n" % (jobname))
+                            jobname = CreateMIPJob(instance, nrback, model="YFix", mipsetting=setting)
+                            filenew.write("sbatch %s \n" % (jobname))
                             jobname = CreateMLLocalSearchJob(instance, nrback, nrforward, setting, model="YFix", mlsetting="NrIterationBeforeTabu1000")
                             filenew.write("sbatch %s \n" % (jobname))
 
-                    #jobname = CreateMLLocalSearchJob(instance, nrback, nrforward, "SymetricMIP", model="YFix",
-                    #                                 mlsetting="NrIterationBeforeTabu1000", sddpsetting = "EvalOutSample")
-                    #filenew.write("sbatch %s \n" % (jobname))
+                    jobname = CreateMLLocalSearchJob(instance, nrback, nrforward, "SymetricMIP", model="YFix",
+                                                     mlsetting="NrIterationBeforeTabu1000", sddpsetting = "EvalOutSample")
+                    filenew.write("sbatch %s \n" % (jobname))
 
 
 
